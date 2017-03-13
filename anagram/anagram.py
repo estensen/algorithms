98406448f206944b2d001f4cf9f37b408f834a37
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-13 15:36:46 +0100

    Add anagram finder

---
+++ anagram/anagram.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import time
+
+def parse_dictionary(filename):
+    word_anagrams = defaultdict(list)
+
+    with open(filename) as dictionary:
+        for word in dictionary:
+            word = word.rstrip()
+            word_anagrams[''.join(sorted(word))] = [word]
+
+    return word_anagrams
+
+def parse_lookup_words(filename):
+    words_lookup = []
+
+    with open(filename) as lookup_words:
+        for word in lookup_words:
+            word = word.rstrip()
+            words_lookup.append(word)
+
+    return words_lookup
+
+def main():
+    start = time.time()
+    word_anagrams = parse_dictionary("vocabulary.txt")
+    words_lookup = parse_lookup_words("test_words.txt")
+
+    for word in words_lookup:
+        sorted_word = ''.join(sorted(word))
+
+        if word_anagrams[sorted_word]:
+            word_anagrams[sorted_word].append(word)
+    for words in sorted(word_anagrams.values()):
+        if len(words) > 1:
+            print(', '.join(words))
+
+    end = time.time()
+    print(end - start)
+
+if __name__ == "__main__":
+    main()
