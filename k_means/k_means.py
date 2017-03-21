d5dca5fc1c724a6d89f726dff8fff24c7aea7e33
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-21 22:42:24 +0100

    Compute distance correctly

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -1,34 +1,30 @@
-def k_means(centroids, data_points):
-    distances = [data_points[:] for x in range(len(centroids))]
-    print("Data points: {0}".format(data_points))
-
-    # TODO: Stop at conergence
+def k_means(centroids, data_points, data_poin_names):
+    counter = 0
     while True:
         # Compute distances
-        distances = compute_distances(centroids, distances)
+        distances = compute_distances(centroids, data_points)
 
         # Assign each point to closest centroid
         assigned_points = [[] for x in centroids]
         assigned_points = assign_points(assigned_points, distances)
 
         # Recompute centroids
-        print("Recomputing centroids...")
+        print("Iteration {0}:".format(counter))
         prev_centroids = centroids[:]
-        #print("prev_centroids: {0}".format(prev_centroids))
         centroids = compute_centroids(centroids, assigned_points, data_points)
-        #print("centroids: {0}".format(centroids))
-        print(assigned_points)
         for i, item in enumerate(centroids):
-            print("Centroid {0}".format(item))
+            print()
+            print("Cluster #{0}".format(i))
+            print("Old centroid {0}".format(prev_centroids[i]))
             #print("Distances to points: {0}".format(i))
             print("Points closest: {0}".format(assigned_points[i]))
-        print("...")
+            print("New centroid {0}".format(item))
+        print()
 
         if set(prev_centroids) == set(centroids):
-            print("Converging on: {0}".format(centroids))
-
+            print("There is convergence!")
             break
-
+        counter += 1
 
 def compute_centroids(centroids, assigned_points, data_points):
     for i, centroid in enumerate(centroids):
@@ -45,29 +41,34 @@ def assign_points(assigned_points, distances):
         assigned_points[point.index(min(point))].append(z)
     return assigned_points
 
-def compute_distances(centroids, distances):
+def compute_distances(centroids, data_points):
+    distances = [data_points[:] for x in range(len(centroids))]
     for i, centroid in enumerate(centroids):
         for j, distance in enumerate(distances[i]):
             # print("abs({0} - {1}) = {2}".format(distance, centroid, abs(distance - centroid)))
             distances[i][j] = abs(distance - centroid)
     return distances
 
-def read_data(filename):
+def read_data(filename, i):
     with open(filename, 'r') as f:
         data = f.read()
 
     lines = data.split('\n')
     data_points = []
     for line in lines:
-        line = line.split(';')[1]
-        data_points.append(int(line))
+        line = line.split(';')[i]
+        data_points.append(line)
     return data_points
 
 def main():
     # Initial centroids
     centroids = [4, 5, 8]
     print("Initial centroids: {0}".format(centroids))
-    data_points = read_data("data.txt")
-    k_means(centroids, data_points)
+    data_points = read_data("data.txt", 1)
+    data_points = [int(i) for i in data_points]
+    data_point_names = read_data("data.txt", 0)
+    print("Data points:")
+    print(*("{0}".format(p) for p in zip(data_points, data_point_names)))
+    k_means(centroids, data_points, data_point_names)
 
 main()
