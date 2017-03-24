b5f47a4a7047ff8dfe04ded581e1270d70a62049
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-24 11:56:18 +0100

    Add deque implementation of palindrome checker

---
+++ palindrome/palindrome.py
@@ -0,0 +1,18 @@
+from data_structures.deque import Deque
+
+
+def palindrome(text_string):
+    char_deque = Deque()
+
+    for c in text_string:
+        char_deque.add_rear(c)
+
+    still_equal = True
+
+    while char_deque.size() > 1 and still_equal:
+        first_c = char_deque.remove_front()
+        last_c = char_deque.remove_rear()
+        if first_c != last_c:
+            still_equal = False
+
+    return still_equal
