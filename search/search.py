9227db5c6dd321a36c3d97fea73c27283c2118c0
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-27 09:46:55 +0200

    Add sequential search

---
+++ search/search.py
@@ -0,0 +1,5 @@
+from sequential_search import SequentialSearch
+
+test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print(SequentialSearch(test_list, 3).search())
+print(SequentialSearch(test_list, 13).search())
