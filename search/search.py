23de1608abb3eabfb2a80c527db747b02ef414ff
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-28 11:57:18 +0200

    Move sequential search to search class

--- search/search.py
+++ search/search.py
@@ -1,5 +1,20 @@
-from sequential_search import SequentialSearch
+class Search:
+    def __init__(self, search_space, item):
+        self.search_space = search_space
+        self.item = item
+
+    def sequential_search(self):
+        pos = 0
+        found = False
+
+        while pos < len(self.search_space) and not found:
+            if self.search_space[pos] == self.item:
+                found = True
+            else:
+                pos += 1
+
+        return found
 
 test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(SequentialSearch(test_list, 3).search())
-print(SequentialSearch(test_list, 13).search())
+print(Search(test_list, 3).sequential_search())
+print(Search(test_list, 13).sequential_search())
