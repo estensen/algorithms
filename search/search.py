fd7114524fe4444c12def71a0ccd516ede848f81
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-28 15:51:23 +0200

    Make binary search recursive

--- search/search.py
+++ search/search.py
@@ -1,42 +1,35 @@
 class Search:
-    def __init__(self, search_space, item):
-        self.search_space = search_space
-        self.item = item
-
-    def sequential_search(self):
+    def sequential_search(self, search_space, item):
         pos = 0
         found = False
 
-        while pos < len(self.search_space) and not found:
-            if self.search_space[pos] == self.item:
+        while pos < len(search_space) and not found:
+            if search_space[pos] == item:
                 found = True
             else:
                 pos += 1
 
         return found
 
-    def binary_search(self):
-        first = 0
-        last = len(self.search_space) - 1
-        found = False
-
-        while first <= last and not found:
-            midpoint = (first + last) // 2
-            if self.search_space[midpoint] == self.item:
-                found = True
+    def binary_search(self, sorted_search_space, item):
+        if len(sorted_search_space) == 0:
+            return False
+        else:
+            midpoint = len(sorted_search_space) // 2
+            if sorted_search_space[midpoint] == item:
+                return True
             else:
-                if self.item < self.search_space[midpoint]:
-                    last = midpoint - 1
+                if item < sorted_search_space[midpoint]:
+                    return self.binary_search(sorted_search_space[:midpoint], item)
                 else:
-                    first = midpoint + 1
-
-        return found
+                    return self.binary_search(sorted_search_space[midpoint+1:], item)
 
 test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
 # Sequential search
-print(Search(test_list, 3).sequential_search())
-print(Search(test_list, 13).sequential_search())
+s = Search()
+print(s.sequential_search(test_list, 3))
+print(s.sequential_search(test_list, 13))
 test_list2 = [0, 1, 2, 8, 13, 17, 19, 32, 42]
 # Binary search
-print(Search(test_list2, 2).binary_search())
-print(Search(test_list2, 18).binary_search())
+print(s.binary_search(test_list2, 2))
+print(s.binary_search(test_list2, 18))
