ac10e6033410a10079002c79aceb9160a7a2dbb4
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-31 14:17:59 +0200

    Stop comparing bubbles when encountering larger bubble

--- sort/sort.py
+++ sort/sort.py
@@ -1,11 +1,13 @@
 def bubble_sort(data):
     data_length = len(data)
-    for i in range(data_length):
-        for j in range(data_length - 1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
-
-
+    while True:
+        swapped = False
+        for i in range(data_length - 1):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                swapped = True
+        if not swapped:
+            break
 
 unsorted_list = [54,26,93,17,77,31,44,55,20]
 bubble_sort(unsorted_list)
